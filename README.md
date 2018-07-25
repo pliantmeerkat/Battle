69f731a821f7b95356c71ebbf5b1c383cf6a4947
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-25 13:52:52 +0100

    middle of day 3

--- README.md
+++ README.md
@@ -12,4 +12,4 @@
 
 ### Instructions
 
-## Cretits
+## Credits
--- app.rb
+++ app.rb
@@ -1,8 +1,38 @@
 require 'sinatra'
+require "securerandom"
+require './lib/player'
+require './lib/game'
 # Battle
 class Battle < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+
   get '/' do
-    'Battle'
+    erb(:index)
   end
+
+  post '/names' do
+    $game = Game.new(Player.new(params[:player_1]), Player.new(params[:player_2]))
+    redirect '/play'
+  end
+
+  get '/attack' do
+    @game = $game
+    @game.attack(@game.players[1])
+    erb(:attack)
+  end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
+  end
+
+  get '/play' do
+    @game = $game
+    erb(:play)
+  end
+
   run! if app_file == $PROGRAM_NAME
+
 end
